3dbf40970208d735ccb93e2f8b96f56a3a3a1e40
Mostafa ElHelaly <155623835+Mostafa-ElHelaly@users.noreply.github.com>
2024-07-23 10:00:40 +0300

    23/7/2024

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    ConfigSize().init(context);
+
     return MultiBlocProvider(
       providers: [
         BlocProvider(
