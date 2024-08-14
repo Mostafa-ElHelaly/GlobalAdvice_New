cbe4075b91a03150ca9f4d409d4ed465d5f9ea69
yossefJoe <75579835+yossefJoe@users.noreply.github.com>
2024-08-14 06:00:10 -0700

    backup1

--- lib/features/home/data/repo_imp/repo_impl.dart
+++ lib/features/home/data/repo_imp/repo_impl.dart
@@ -11,6 +11,7 @@ import 'package:globaladvice_new/features/home/data/model/property_dependinces_m
 import 'package:globaladvice_new/features/home/data/model/property_model.dart';
 import 'package:globaladvice_new/features/home/data/model/property_policy_request_model.dart';
 import 'package:globaladvice_new/features/home/domain/repo/homeBaseRepo.dart';
+import 'package:globaladvice_new/features/home/presentation/manager/property_data_bloc/property_data_bloc.dart';
 
 import '../../../../core/error/failure.dart';
 import '../../../../core/utils/api_helper.dart';
