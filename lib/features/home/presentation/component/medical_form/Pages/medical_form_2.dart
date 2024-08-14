93af35ce981498a42c8883a01745c2846afc30c7
yossefJoe <75579835+yossefjoe@users.noreply.github.com>
2024-08-13 23:46:37 -0700

    finishglobal

--- lib/features/home/presentation/component/medical_form/Pages/medical_form_2.dart
+++ lib/features/home/presentation/component/medical_form/Pages/medical_form_2.dart
@@ -126,7 +126,7 @@ class _MedicalForm2State extends State<MedicalForm2> {
   Widget build(BuildContext context) {
     List<String> siblingrelations = [
       AppLocalizations.of(context)!.spouse,
-      AppLocalizations.of(context)!.children,
+      AppLocalizations.of(context)!.child,
     ];
     List<String> genderstype = [
       AppLocalizations.of(context)!.male,
@@ -172,9 +172,10 @@ class _MedicalForm2State extends State<MedicalForm2> {
     }
 
     List<String> modify_genders_list() {
-      genders =
-          genderselectedValues.map((e) => return_gender(e.toString())).toList();
-      genders!.insert(0, return_gender(selectedValue!));
+      genders = genderselectedValues
+          .map((e) => return_gender(e.toString().toLowerCase()))
+          .toList();
+      genders!.insert(0, return_gender(selectedValue!.toLowerCase()));
       return genders!;
     }
 
